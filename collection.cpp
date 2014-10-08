7ec50a0c40788c5e2ef271821461721a5db25628
Massimo Gengarelli <massimo.gengarelli@gmail.com>
2014-10-08 11:45:51 +0200

    Convert to lambda functions

--- collection.cpp
+++ collection.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iomanip>
 #include <fstream>
+#include <algorithm>
 
 using namespace todo;
 
@@ -24,39 +25,37 @@ collection::~collection()
 
 std::ostream & collection::serialize(std::ostream & p_stream) const
 {
-	const uint32_t l_separator = 0xDEADBEEF;
-	const_iterator l_iterator = begin();
+  const uint32_t l_separator = 0xDEADBEEF;
+  uint64_t l_size = size();
 
-	uint64_t l_size = size();
+  p_stream << l_size;
 
-	p_stream << l_size;
+  std::for_each(begin(), end(), [&](todo::element const & l_element)->void {
+    l_element.serialize(p_stream);
+    p_stream << l_separator;
+  });
 
-	for (; l_iterator != end(); ++l_iterator) {
-		l_iterator->serialize(p_stream);
-		p_stream << l_separator;
-	}
-
-	return p_stream;
+  return p_stream;
 }
 
 std::istream & collection::deserialize(std::istream & p_stream)
 {
-	uint32_t l_separator;
+  uint32_t l_separator;
 
-	uint64_t l_size(0);
-	p_stream >> l_size;
+  uint64_t l_size(0);
+  p_stream >> l_size;
 
-	for (uint32_t l_iter = 0; l_iter < l_size; l_iter++)
-	{
-		todo::element l_element;
-		l_element.deserialize(p_stream);
+  for (uint32_t l_iter = 0; l_iter < l_size; l_iter++)
+  {
+    todo::element l_element;
+    l_element.deserialize(p_stream);
     l_element.m_index = l_iter;
-		push_back(l_element);
+    push_back(l_element);
 
-		p_stream >> l_separator;
-	}
+    p_stream >> l_separator;
+  }
 
-	return p_stream;
+  return p_stream;
 }
 
 void collection::write_file() const
@@ -102,13 +101,11 @@ todo::collection const & collection::sort_by_priority(bool ascendent)
 
     m_sorted = new collection();
 
-    const_iterator l_iterator = begin();
-    for (; l_iterator != end(); ++l_iterator)
-    {
-      if (l_iterator->m_priority == 0) l_low.push_back_original(*l_iterator);
-      else if (l_iterator->m_priority == 1) l_medium.push_back_original(*l_iterator);
-      else l_high.push_back_original(*l_iterator);
-    }
+    std::for_each(begin(), end(), [&](todo::element & l_element)->void {
+      if (l_element.m_priority == 0) l_low.push_back_original(l_element);
+      else if (l_element.m_priority == 1) l_medium.push_back_original(l_element);
+      else l_high.push_back_original(l_element);
+    });
 
     if (not ascendent)
       m_sorted->insert(m_sorted->end(), l_high.begin(), l_high.end());
